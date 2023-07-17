0a36277cc69f30c8a0ef8a2514f23fcfd59a715f
Richy <richy@webcomm.com.tw>
2023-07-17 21:39:36 +0800

    connect Action, sender.value 取得 Slider值

--- iOS_HelloUISlider/ViewController.swift
+++ iOS_HelloUISlider/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        myLabel.text =  "\(Int(sender.value))" // Float to Int to String
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
